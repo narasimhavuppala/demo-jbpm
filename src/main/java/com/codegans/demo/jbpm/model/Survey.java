92e7aaf264172da6eb5c4a8ef87ee1f51bcd01d8
Victor Polischuk <victor2@ukr.net>
2017-10-17 11:50:22 +0300

    Made some customizations for packages and classes

--- src/main/java/com/codegans/demo/jbpm/model/Survey.java
+++ src/main/java/com/codegans/demo/jbpm/model/Survey.java
@@ -6,6 +6,53 @@ package com.codegans.demo.jbpm.model;
  * @author Victor Polischuk
  * @since 17.10.2017 10:08
  */
-public class Survey {
+public class Survey implements java.io.Serializable {
+    private Boolean satisfied;
+    private Boolean deliveredOnTime;
+    private String missingEquipment;
+    private String comment;
 
+    public Survey() {
+    }
+
+    public Survey(Boolean satisfied,
+                  Boolean deliveredOnTime,
+                  String missingEquipment, String comment) {
+        this.satisfied = satisfied;
+        this.deliveredOnTime = deliveredOnTime;
+        this.missingEquipment = missingEquipment;
+        this.comment = comment;
+    }
+
+    public Boolean getSatisfied() {
+        return this.satisfied;
+    }
+
+    public void setSatisfied(Boolean satisfied) {
+        this.satisfied = satisfied;
+    }
+
+    public Boolean getDeliveredOnTime() {
+        return this.deliveredOnTime;
+    }
+
+    public void setDeliveredOnTime(Boolean deliveredOnTime) {
+        this.deliveredOnTime = deliveredOnTime;
+    }
+
+    public String getMissingEquipment() {
+        return this.missingEquipment;
+    }
+
+    public void setMissingEquipment(String missingEquipment) {
+        this.missingEquipment = missingEquipment;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
