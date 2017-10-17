e42c31024211bd48496115560fb5cf83d1845fce
Victor Polischuk <victor2@ukr.net>
2017-10-17 17:39:00 +0300

    Added kie-spring to action

--- src/main/java/com/codegans/demo/jbpm/ItOrderRestController.java
+++ src/main/java/com/codegans/demo/jbpm/ItOrderRestController.java
@@ -1,6 +1,13 @@
 package com.codegans.demo.jbpm;
 
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * JavaDoc here
@@ -10,8 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ItOrderRestController {
+//    private final KieBase knowledgeBase;
+    private final RuntimeEngine runtimeEngine;
 
+//    public ItOrderRestController(KieBase knowledgeBase) {
+//        this.knowledgeBase = knowledgeBase;
+//    }
+
+    public ItOrderRestController(RuntimeEngine runtimeEngine) {
+        this.runtimeEngine = runtimeEngine;
+    }
+
+    @RequestMapping("/order/create")
     public String create() {
+        Map<String,Object> data = new HashMap<>();
+
+//        KieSession session = knowledgeBase.newKieSession();
+        KieSession session = runtimeEngine.getKieSession();
+
+        try {
+            ProcessInstance processInstance = session.startProcess("itorders.orderhardware", data);
+
+            System.out.println(processInstance);
+        } finally {
+            session.dispose();
+        }
+        
         return null;
     }
 }
--- src/main/java/com/codegans/demo/jbpm/JbpmConfiguration.java
+++ src/main/java/com/codegans/demo/jbpm/JbpmConfiguration.java
@@ -1,10 +1,23 @@
 package com.codegans.demo.jbpm;
 
 import org.kie.api.KieBase;
+import org.kie.api.io.Resource;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironment;
+import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.runtime.manager.context.ProcessInstanceIdContext;
 import org.kie.internal.utils.KieHelper;
+import org.kie.spring.factorybeans.RuntimeEnvironmentFactoryBean;
+import org.kie.spring.factorybeans.RuntimeManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Map;
 
 /**
  * JavaDoc here
@@ -12,15 +25,46 @@ import org.springframework.context.annotation.Configuration;
  * @author Victor Polischuk
  * @since 16.10.2017 23:06
  */
+@EnableTransactionManagement
 @Configuration
 public class JbpmConfiguration {
     @Bean
     public KieBase createKnowledgeBase() {
-        System.out.println("Createing KB");
+        System.out.println("Creating KB");
 
         return new KieHelper()
-                .addResource(ResourceFactory.newClassPathResource("bpmn/place-order.bpmn2.xml"))
-                .addResource(ResourceFactory.newClassPathResource("bpmn/hardware-order.bpmn2.xml"))
+                .addResource(ResourceFactory.newClassPathResource("bpmn/place-order.bpmn2.xml"), ResourceType.BPMN2)
+                .addResource(ResourceFactory.newClassPathResource("bpmn/hardware-order.bpmn2.xml"), ResourceType.BPMN2)
                 .build();
+    }
+
+    @Bean
+    public RuntimeEnvironmentFactoryBean createRuntimeEnvironment(EntityManagerFactory entityManagerFactory, PlatformTransactionManager transactionManager) {
+        RuntimeEnvironmentFactoryBean runtimeEnvironmentFactoryBean = new RuntimeEnvironmentFactoryBean();
+
+        Map<Resource, ResourceType> assets = runtimeEnvironmentFactoryBean.getAssets();
+        
+        assets.put(ResourceFactory.newClassPathResource("bpmn/place-order.bpmn2.xml"), ResourceType.BPMN2);
+        assets.put(ResourceFactory.newClassPathResource("bpmn/hardware-order.bpmn2.xml"), ResourceType.BPMN2);
+
+        runtimeEnvironmentFactoryBean.setEntityManagerFactory(entityManagerFactory);
+        runtimeEnvironmentFactoryBean.setTransactionManager(transactionManager);
+
+        return runtimeEnvironmentFactoryBean;
+    }
+
+    @Bean
+    public RuntimeManagerFactoryBean createRuntimeManager(RuntimeEnvironment runtimeEnvironment) {
+        RuntimeManagerFactoryBean runtimeManagerFactoryBean = new RuntimeManagerFactoryBean();
+
+        runtimeManagerFactoryBean.setRuntimeEnvironment(runtimeEnvironment);
+        runtimeManagerFactoryBean.setIdentifier("com.codegans.demo.jbpm");
+
+        return runtimeManagerFactoryBean;
+    }
+
+    @Bean
+    public RuntimeEngine createRuntimeEngine(RuntimeManager runtimeManager) {
+        return runtimeManager.getRuntimeEngine(ProcessInstanceIdContext.get());
     }
 }
